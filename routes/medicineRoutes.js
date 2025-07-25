20bb35d27868119e35322d29c49162041f1b5316
Irshad Ahmad <irshad1554@gmail.com>
2025-07-25 19:38:40 +0530

    added few function

--- routes/medicineRoutes.js
+++ routes/medicineRoutes.js
@@ -7,12 +7,14 @@ import {
   deleteMedicine
 } from '../controllers/medicineController.js';
 
+import { protect, admin } from '../middleware/authMiddleware.js';
+
 const router = express.Router();
 
 router.get('/', getAllMedicines);
 router.get('/:id', getMedicineById);
-router.post('/create-medicine', createMedicine);
-router.put('/:id', updateMedicine);
-router.delete('/:id', deleteMedicine);
+router.post('/create-medicine', protect, admin, createMedicine);
+router.put('/edit/:id', protect, admin, updateMedicine);
+router.delete('/:id', protect, admin, deleteMedicine);
 
 export default router;
